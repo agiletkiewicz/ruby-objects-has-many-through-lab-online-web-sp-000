459f22acb3e202af806bc3d0253ebfb863486104
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-05-03 18:05:53 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,6 +1,19 @@
 class Doctor 
   
+  attr_reader :name
   
+  @@all = []
   
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def new_appointment(date, patient)
+  end
   
 end
